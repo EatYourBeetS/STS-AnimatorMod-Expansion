2cad5f3a4e9264b0f13988e0e2cbb9adc3defdcb
NavanBethrax <syskiller1337@hotmail.com>
2019-07-28 05:06:04 +0200

    Jojo basic skills
    Changed Strike_Jojo and added Defend_Jojo.

--- EatYourBeetSVG/src/main/java/expansion/cards/animator/Strike_Jojo.java
+++ EatYourBeetSVG/src/main/java/expansion/cards/animator/Strike_Jojo.java
@@ -1,11 +1,11 @@
 package expansion.cards.animator;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import eatyourbeets.utilities.GameActionsHelper;
+import eatyourbeets.powers.PlayerStatistics;
 
 public class Strike_Jojo extends Strike
 {
@@ -15,22 +15,24 @@ public class Strike_Jojo extends Strike
     {
         super(ID, 1, CardTarget.ENEMY);
 
-        Initialize(6,0, 4);
+        Initialize(6,0, 1);
     }
 
-    @Override
-    public void triggerOnManualDiscard()
-    {
-        super.triggerOnManualDiscard();
-
-        AbstractPlayer p = AbstractDungeon.player;
-        GameActionsHelper.DamageRandomEnemy(p, this.magicNumber, DamageInfo.DamageType.THORNS, AbstractGameAction.AttackEffect.BLUNT_LIGHT);
-    }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) 
     {
         GameActionsHelper.DamageTarget(p, m, this.damage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.BLUNT_LIGHT);
+    }
+
+    @Override
+    public void triggerOnExhaust()
+    {
+        super.triggerOnExhaust();
+
+        AbstractPlayer p = AbstractDungeon.player;
+        PlayerStatistics.ApplyTemporaryDexterity(p, p, 1);
+
     }
 
     @Override
