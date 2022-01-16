a657506a859d5e445df679389ae8332ad83e4e22
SevenDayCandle <persisting_delusions@yahoo.com>
2022-01-15 17:19:44 -0800

    - Repurposing this repository's main branch for the new Animator Redesign Beta Addon

--- src/main/java/eatyourbeets/utilities/BetaGameUtilities.java
+++ src/main/java/eatyourbeets/utilities/BetaGameUtilities.java
@@ -8,6 +8,8 @@ import static eatyourbeets.resources.GR.Enums.CardTags.HASTE;
 
 public class BetaGameUtilities extends GameUtilities
 {
+    // TODO Convenience Function for adding tags to cards
+    // TODO I would also like to add support for an Autoplay tag, which I see has been defined in GR.Enums. However, I'm not sure if it is actually used (i.e. Essence_Eruza doesn't use this tag)
     public static void ModifyCardTag(AbstractCard card, AbstractCard.CardTags tag, boolean value)
     {
         if (tag == null || card == null) {
@@ -15,11 +17,17 @@ public class BetaGameUtilities extends GameUtilities
         }
         EYBCard eCard = JUtils.SafeCast(card, EYBCard.class);
         if (eCard != null) {
-            if (DELAYED.equals(tag)) {
+            if (PURGE.equals(tag)) {
+                eCard.SetPurge(value);
+            }
+            else if (DELAYED.equals(tag)) {
                 eCard.SetDelayed(value);
             }
             else if (HASTE.equals(tag)) {
                 eCard.SetHaste(value);
+            }
+            else if (LOYAL.equals(tag)) {
+                eCard.SetLoyal(value);
             }
             else if (value) {
                 eCard.tags.add(tag);
